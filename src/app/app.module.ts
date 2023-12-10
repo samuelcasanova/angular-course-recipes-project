52d136efe0676484510289f95e9a4d3ecece6a16
samuel.casanova <samuel.casanova@immfly.com>
2023-12-10 10:13:39 +0100

    add dropdown custom attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.compo
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { DropdownDirective } from './common/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/common/dropdown.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, HostBinding, HostListener } from "@angular/core";
+
+@Directive({ selector: '[appDropdown]' })
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false
+
+  @HostListener('click') toggleOpen(event: Event) {
+    this.isOpen = !this.isOpen
+  }
+}
