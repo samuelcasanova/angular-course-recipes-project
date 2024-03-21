1ba914c5d4857caa6bd981ba2e7061db08dea1a8
samuel.casanova <samuel.casanova@immfly.com>
2024-03-21 07:51:20 +0100

    add logout functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
   userSubject = new BehaviorSubject<User | null>(null)
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   signUp(email: string, password: string) {
     const payload = { email, password, returnSecureToken: true }
@@ -21,6 +22,11 @@ export class AuthService {
     return this.httpClient
       .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBu0_c21_FWKHGpNytOYFfUNwXUQLUMq7Y', payload)
       .pipe(catchError(this.handleError.bind(this)), tap(this.handleAuthentication.bind(this)))
+  }
+
+  logout() {
+    this.userSubject.next(null)
+    this.router.navigate(['/auth'])
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -30,4 +30,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   onFetchData() {
     this.dataStorageService.fetchData().subscribe()
   }
+
+  onLogout() {
+    this.authService.logout()
+  }
 }
